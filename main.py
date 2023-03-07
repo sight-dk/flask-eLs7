c97dc3e0ae4d2a0637239cbd6111e2ec0f870f75
sight-dk <kontakt@sight.dk>
2023-03-07 15:53:07 +0100

    second

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
-# Import flask and datetime module for showing date and time
-from flask import Flask
+import uuid
+import hashlib
 import datetime
-  
-x = datetime.datetime.now()
-  
-# Initializing flask app
+from flask import Flask, request, jsonify
+import psycopg2
+
 app = Flask(__name__)
-  
-  
-# Route for seeing a data
-@app.route('/data')
-def get_time():
-  
-    # Returning an api for showing in  reactjs
-    return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
-        }
-  
-      
-# Running app
+
+# Postgres database connection
+conn = psycopg2.connect(
+    host="containers-us-west-36.railway.app",
+    database="railway",
+    user="postgres",
+    password="QI1B5PcTFIekHXgUjjzo"
+)
+
+# Cursor
+cur = conn.cursor()
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    firstname = data['firstname']
+    lastname = data['lastname']
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    try:
+        cur.execute("INSERT INTO users (username, password, firstname, lastnmae) VALUES (%s, %s, %s, %s)", (username, hashed_password, firstname, lastname))
+        conn.commit()
+        return jsonify({'success': True})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    try:
+        cur.execute("SELECT id FROM users WHERE username=%s AND password=%s", (username, hashed_password))
+        user_id = cur.fetchone()
+        if user_id:
+            token = str(uuid.uuid4())
+            expiration = datetime.datetime.now() + datetime.timedelta(hours=1)
+            cur.execute("INSERT INTO sessions (user_id, token, expiration) VALUES (%s, %s, %s)", (user_id[0], token, expiration))
+            conn.commit()
+            return jsonify({'success': True, 'token': token})
+        else:
+            return jsonify({'success': False, 'error': 'Invalid username or password'})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
+
 if __name__ == '__main__':
     app.run(debug=True)
+

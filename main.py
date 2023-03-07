9c9b262d965649a3dd3b6daa4d27da2d5619bea8
sight-dk <kontakt@sight.dk>
2023-03-07 15:24:24 +0100

    first

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask, jsonify
-import os
-
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+  
+x = datetime.datetime.now()
+  
+# Initializing flask app
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
+  
+  
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+  
+    # Returning an api for showing in  reactjs
+    return {
+        'Name':"geek", 
+        "Age":"22",
+        "Date":x, 
+        "programming":"python"
+        }
+  
+      
+# Running app
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
